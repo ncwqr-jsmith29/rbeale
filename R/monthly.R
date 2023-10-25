c208e7f669d182508d10d8a7a51fa9c4c4aa4592
Colleen Cosgrove <ccosgrov@heidelberg.edu>
2023-10-25 14:21:34 -0400

    Got all files to print correctly for the monthly code; updated the monthly file and deleted the updated file, since it's all working now.

--- R/monthly.R
+++ R/monthly.R
@@ -52,7 +52,58 @@ monthly <- function(parent_directory, input_rivers){
                                    s10 = numeric())
 
 
+
     for(variable in variables){
+
+      ##Build data frames for output files
+      best_individuals_all <- data.frame(s1 = numeric(),
+                                         s2 = numeric(),
+                                         s3 = numeric(),
+                                         s4 = numeric(),
+                                         s5 = numeric(),
+                                         s6 = numeric(),
+                                         s7 = numeric(),
+                                         s8 = numeric(),
+                                         s9 = numeric(),
+                                         s10 = numeric())
+      best_strata_mses_all <- data.frame(strata = numeric(),
+                                         strata.n = numeric(),
+                                         dload.bi_kgd = numeric(),
+                                         dload.ub_kgd = numeric(),
+                                         tload_kglenS = numeric(),
+                                         degf = numeric(),
+                                         lenObs = numeric(),
+                                         lenStr = numeric(),
+                                         MSE_kglenS = numeric(),
+                                         Cl_kglenS = numeric(),
+                                         flowmu_m3s = numeric(),
+                                         flowav_m3s = numeric()) #mse.m_lowest_all_strata #all_strata_best_mse
+      best_individual_mses_all <- data.frame(strata = numeric(),
+                                             dload.bi_kgd = numeric(),
+                                             dload.ub_kgd = numeric(),
+                                             tload_kglenS = numeric(),
+                                             degf = numeric(),
+                                             lenObs = numeric(),
+                                             lenStr = numeric(),
+                                             MSE_kglenS = numeric(),
+                                             Cl_kglenS = numeric(),
+                                             flowmu_m3s = numeric(),
+                                             flowav_m3s = numeric()) #mse.m_lowest_summary #best_summary_mses
+      individual_mses_all <- data.frame(strata = numeric(),
+                                        dload.bi_kgd = numeric(),
+                                        dload.ub_kgd = numeric(),
+                                        tload_kglenS = numeric(),
+                                        degf = numeric(),
+                                        lenObs = numeric(),
+                                        lenStr = numeric(),
+                                        MSE_kglenS = numeric(),
+                                        Cl_kglenS = numeric(),
+                                        flowmu_m3s = numeric(),
+                                        flowav_m3s = numeric())
+      out.year=data.frame(yearmo = numeric())
+      out.year.mult = data.frame(yearmo = numeric())
+      out.year.mult.bim = data.frame(yearmo = numeric())
+      out.year.mult.bi = data.frame(yearmo = numeric())
 
       for(yearmo in yearmos){
 
@@ -115,112 +166,163 @@ monthly <- function(parent_directory, input_rivers){
         if(all(!is.na(obs_data[,variable]))){
           strata_mses <- sbeale(obs_data$Flow, obs_data[, variable])
           strata_mses[7] <- strata_mses[7] * N^2
-          best_strata_mses[1,] <- c(1, 1, strata_mses)
-          best_individual_mses[1,] <- c(1, strata_mses)
+          best_strata_mses[1,] <- c(0, 1, strata_mses)
+          best_individual_mses[1,] <- c(0, strata_mses)
 
           print(paste("Running sbeale for", variable, "...", yearmo, sep = " "))
+
+
+          one_individual_mse<- c(0, strata_mses)#format for output files
+
         }else{
 
-        for(strata in try_strata){
-          print(paste("Processing", strata, "strata for", variable, "...", yearmo, sep = " "))
-          if(strata == 1){
-            popSize <- 1
-            pop <- c(1,last.allele)
-            strata_mses <- sbeale(obs_data$Flow, obs_data[, variable])
-            strata_mses[7] <- strata_mses[7] * N^2
-            best_strata_mses[1,] <- c(1, 1, strata_mses)
-            best_individual_mses[1,] <- c(1, strata_mses)
-          }else{#if strata >= 2
+          for(strata in try_strata){
+            print(paste("Processing", strata, "strata for", variable, "...", yearmo, sep = " "))
+            if(strata == 1){
+              popSize <- 1
+              pop <- c(1,last.allele)
+              strata_mses <- sbeale(obs_data$Flow, obs_data[, variable])
+              strata_mses[7] <- strata_mses[7] * N^2
+              best_strata_mses[1,] <- c(1, 1, strata_mses)
+              best_individual_mses[1,] <- c(1, strata_mses)
+            }else{#if strata >= 2
 
-            big_combos = create_monthly_combos(strata, last.allele, n.cores)
+              big_combos = create_monthly_combos(strata, last.allele, n.cores)
 
-            variable.data <- dplyr::pull(obs_data, variable)
-            problem_individuals <- logical()
-            parallel::clusterExport(cl, list("find_problem_individuals", "problem_individuals", "variable.data", "strata"), envir = environment())
-            problem_individuals <- parallel::parRapply(cl, big_combos, find_problem_individuals)
-            #eliminate problem individuals
-            big_combos <- big_combos[!problem_individuals,]
-
-
-            if(class(big_combos)[1] == "numeric"){
-              #this means there is 1 individual, so the matrix is a vector
-              #force it to be a matrix
-              big_combos <- matrix(big_combos, nrow = 1)
-            }
-
-            popSize <- nrow(big_combos)
-
-            #if there are no good individuals with 2 strata, then there won't be any for 3 or more,
-            #so break out of strata for-loop and move on to next variable
-            if(popSize == 0){
-              print(paste("Not enough data for ", strata, " strata", sep = ""))
-              break
-            }
-
-            pop = big_combos
-
-            strata_mses <- data.frame(dload.bi_kgd = numeric(),
-                                      dload.ub_kgd = numeric(),
-                                      tload_kglenS = numeric(),
-                                      degf = numeric(),
-                                      lenObs = numeric(),
-                                      lenStr = numeric(),
-                                      MSE_kglenS = numeric(),
-                                      Cl_kglenS = numeric(),
-                                      flowmu_m3s = numeric(),
-                                      flowav_m3s = numeric())
-            pop_strata_mses <- vector(mode = "list", length = popSize)
-            names(pop_strata_mses) <- 1:popSize
+              variable.data <- dplyr::pull(obs_data, variable)
+              problem_individuals <- logical()
+              parallel::clusterExport(cl, list("find_problem_individuals", "problem_individuals", "variable.data", "strata"), envir = environment())
+              problem_individuals <- parallel::parRapply(cl, big_combos, find_problem_individuals)
+              #eliminate problem individuals
+              big_combos <- big_combos[!problem_individuals,]
 
 
-            pop_strata_mses <- apply(pop, 1, calc_strata_mses, strata = strata, obs_data = obs_data, variable = variable, strata_mses = strata_mses)
+              if(class(big_combos)[1] == "numeric"){
+                #this means there is 1 individual, so the matrix is a vector
+                #force it to be a matrix
+                big_combos <- matrix(big_combos, nrow = 1)
+              }
+
+              popSize <- nrow(big_combos)
+
+              #if there are no good individuals with 2 strata, then there won't be any for 3 or more,
+              #so break out of strata for-loop and move on to next variable
+              if(popSize == 0){
+                print(paste("Not enough data for ", strata, " strata", sep = ""))
+                break
+              }
+
+              pop = big_combos
+
+              strata_mses <- data.frame(dload.bi_kgd = numeric(),
+                                        dload.ub_kgd = numeric(),
+                                        tload_kglenS = numeric(),
+                                        degf = numeric(),
+                                        lenObs = numeric(),
+                                        lenStr = numeric(),
+                                        MSE_kglenS = numeric(),
+                                        Cl_kglenS = numeric(),
+                                        flowmu_m3s = numeric(),
+                                        flowav_m3s = numeric())
+              pop_strata_mses <- vector(mode = "list", length = popSize)
+              names(pop_strata_mses) <- 1:popSize
 
 
-            individual_mses <- do.call(rbind.data.frame, lapply(pop_strata_mses, calc_individual_mse, N))
-            names(individual_mses) <- report_vars
-            individual_mses <- dplyr::mutate(individual_mses, index = 1:nrow(individual_mses), .before = 1)
-
-            lowest_individual_mses <- dplyr::arrange(individual_mses, MSE_kglenS)
-            lowest_individual_indices <- dplyr::pull(lowest_individual_mses, index)
-
-            lowest_individual_mses <- dplyr::select(lowest_individual_mses, -index)
-
-            if(nrow(pop) != 1){
-              pop <- pop[lowest_individual_indices,]
-            }
-            pop_strata_mses <- pop_strata_mses[lowest_individual_indices]
-
-            best_individual <- as.numeric(as.vector(pop[1,]))
-            best_individual <- c(best_individual, rep(NA, 12-strata-1))
-            best_individuals[strata,] <- best_individual
-
-            #best individual mse for this strata
-            best_individual_mses[strata,] <- c(strata, as.numeric(as.vector(lowest_individual_mses[1,])))
-            #best strata mses for this strata
-            add.rows <- cbind(rep(strata, strata), 1:strata, pop_strata_mses[[1]])
-            names(add.rows)[1:2] <- c("strata", "strata.n")
-            best_strata_mses <- dplyr::bind_rows(best_strata_mses, add.rows)
-
-          }#end else
-        }#strata
-        }#sbeale
-
-        one_individual_mse <- dplyr::slice_head(dplyr::arrange(best_individual_mses, MSE_kglenS), n = 1)
+              pop_strata_mses <- apply(pop, 1, calc_strata_mses, strata = strata, obs_data = obs_data, variable = variable, strata_mses = strata_mses)
 
 
-        filename = paste(parent_directory, "/", river, "/Output/Monthly/", variable, "/", variable, "_best_strata_mses_", yearmo, ".csv", sep = '')
-        write.csv(best_strata_mses, filename, row.names = FALSE)
+              individual_mses <- do.call(rbind.data.frame, lapply(pop_strata_mses, calc_individual_mse, N))
+              names(individual_mses) <- report_vars
+              individual_mses <- dplyr::mutate(individual_mses, index = 1:nrow(individual_mses), .before = 1)
 
-        filename = paste(parent_directory, "/", river, "/Output/Monthly/", variable, "/", variable, "_best_individual_mses_", yearmo, ".csv", sep = '')
-        write.csv(best_individual_mses, filename, row.names = FALSE)
+              lowest_individual_mses <- dplyr::arrange(individual_mses, MSE_kglenS)
+              lowest_individual_indices <- dplyr::pull(lowest_individual_mses, index)
 
-        filename = paste(parent_directory, "/", river, "/Output/Monthly/", variable, "/", variable, "_best_", yearmo, ".csv", sep = '')
-        write.csv(one_individual_mse, filename, row.names = FALSE)
+              lowest_individual_mses <- dplyr::select(lowest_individual_mses, -index)
 
-        filename = paste(parent_directory, "/", river, "/Output/Monthly/", variable, "/", variable, "_best_individuals_", yearmo, ".csv", sep = '')
-        write.csv(best_individuals, filename, row.names = FALSE)
+              if(nrow(pop) != 1){
+                pop <- pop[lowest_individual_indices,]
+              }
+              pop_strata_mses <- pop_strata_mses[lowest_individual_indices]
+
+              best_individual <- as.numeric(as.vector(pop[1,]))
+              best_individual <- c(best_individual, rep(NA, 12-strata-1))
+              best_individuals[strata,] <- best_individual
+
+              #best individual mse for this strata
+              best_individual_mses[strata,] <- c(strata, as.numeric(as.vector(lowest_individual_mses[1,])))
+
+              #best strata mses for this strata
+              add.rows <- cbind(strata, strata, 1:strata, pop_strata_mses[[1]])
+              names(add.rows)[1:2] <- c("strata", "strata.n")
+              best_strata_mses <- dplyr::bind_rows(best_strata_mses, add.rows)
+
+            }#end else
+          }#strata
+
+          one_individual_mse <- dplyr::slice_head(dplyr::arrange(best_individual_mses, MSE_kglenS), n = 1)
+
+
+        }#strings of NA
+
+        nstrata <- nrow(best_strata_mses)
+        nstratabestind <- nrow(best_individuals)
+        nstratabestindmses <- nrow(best_individual_mses)
+
+        ##add data to the dataframes whether it's from the sbeale or crossover data
+        individual_mses_all[nrow(individual_mses_all) + 1,] <- one_individual_mse #best_
+
+        best_individuals_all[nrow(best_individuals_all) + nstratabestind,]
+        best_individuals_all<- rbind(best_individuals_all, best_individuals) #best_individuals
+        best_strata_mses_all[nrow(best_strata_mses_all) + nstrata,]
+        best_strata_mses_all<- rbind(best_strata_mses_all, best_strata_mses[,c(1:12)]) #best_strata_mses
+        ###Note: best_strata_mses_all will output NAs for any strata there were not enough data to run
+        best_individual_mses_all[nrow(best_individual_mses_all) + nstratabestindmses,]
+        best_individual_mses_all<- rbind(best_individual_mses_all, best_individual_mses) #best_individual_mses
+
+        #create yearmo output to build final output files
+        #for outputs with more than one strata/more than one potential row per yearmo, use out.year.mult
+        out.year[nrow(out.year) +1,] <- yearmo
+        #build output yearmos for best_strata_mses_all
+        out.year.mult[nrow(out.year.mult) + nstrata,]
+        input=data.frame(rep(yearmo, times=nstrata))
+        out.year.mult<- rbind(out.year.mult, input)
+        #build output yearmos for best ind mses
+        out.year.mult.bim[nrow(out.year.mult.bim) + nstratabestindmses,]
+        input.bim=data.frame(rep(yearmo, times=nstratabestindmses))
+        out.year.mult.bim<- rbind(out.year.mult.bim, input.bim)
+        #build output yearmos for best individuals
+        out.year.mult.bi[nrow(out.year.mult.bi) + nstratabestind,]
+        input.bi=data.frame(rep(yearmo, times=nstratabestind))
+        out.year.mult.bi<- rbind(out.year.mult.bi, input.bi)
+
 
       }#yearmo
+
+      #edit columns names for output
+      names(out.year.mult) <- "yearmo"
+      names(out.year.mult.bim) <- "yearmo"
+      names(out.year.mult.bi) <- "yearmo"
+
+      #add the yearmo into the new output files
+      individual_mses_all <- cbind(out.year, individual_mses_all)
+      best_individuals_all <- cbind(out.year.mult.bi, best_individuals_all)
+      best_individual_mses_all <- cbind(out.year.mult.bim, best_individual_mses_all)
+      best_strata_mses_all <- cbind(out.year.mult, best_strata_mses_all)
+
+      #write file for each variable - output should have 4 total files with all of that variable's data
+      filename = paste(parent_directory, "/", river, "/Output/Monthly/", variable, "/", variable, "_best_strata_mses", ".csv", sep = '')
+      write.csv(best_strata_mses_all, filename, row.names = FALSE)
+
+      filename = paste(parent_directory, "/", river, "/Output/Monthly/", variable, "/", variable, "_best_individual_mses", ".csv", sep = '')
+      write.csv(best_individual_mses_all, filename, row.names = FALSE)
+
+      filename = paste(parent_directory, "/", river, "/Output/Monthly/", variable, "/", variable, "_best", ".csv", sep = '')
+      write.csv(individual_mses_all, filename, row.names = FALSE)
+
+      filename = paste(parent_directory, "/", river, "/Output/Monthly/", variable, "/", variable, "_best_individuals", ".csv", sep = '')
+      write.csv(best_individuals_all, filename, row.names = FALSE)
+
     }#variable
   }#river
 }#function
